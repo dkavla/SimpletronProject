fdb62135a7a11e714648d7a79a6d7123dfd8cd12
dkavla <dkowalczyk135@gmail.com>
2023-06-20 00:45:04 -0400

    Refactor the branching functions to return void and remove array parameter

--- src/operations.h
+++ src/operations.h
@@ -12,61 +12,61 @@ const size_t MEMORY_SPACE = 100; // space in memory; 100 words available
 //  Code = 10
 //  read() => reads a word from keyboard into passed in location 
 //  in memory to store  
-void read(array<int, MEMORY_SPACE>, int& loc);
+void read(array<int, MEMORY_SPACE> mem, int& loc);
 
 //  Code = 11
 //  write() => writes the word location, loc, in memory to the screen
-void write(array<int, MEMORY_SPACE>, int& loc);
+void write(array<int, MEMORY_SPACE> mem, int& loc);
 
 
 
 /*  Load/Store Operations   */
 //  Code = 20
 //  load() => load a word from location in memory into accumulator
-void load(array<int, MEMORY_SPACE>, int& loc, long long int& accum);
+void load(array<int, MEMORY_SPACE> mem, int& loc, long long int& accum);
 
 //  Code = 21
 //  store() => stores a word from the accumulator into a specific location in memory
-void store(array<int, MEMORY_SPACE>, int& loc, long long int& accum);
+void store(array<int, MEMORY_SPACE> mem, int& loc, long long int& accum);
 
 
 /*  Arithmetic Operations   */
 //  Code = 30
 //  add() => add a word from location in memory to the word in the
 //  accumulator (leaving result in accumulator)
-void add(array<int, MEMORY_SPACE>, int& loc, long long int& accum);
+void add(array<int, MEMORY_SPACE> mem, int& loc, long long int& accum);
 
 //  Code = 31
 //  subtract() => subtract a word from location in memory from the
 //  word in accumulator (leaving result in accumulator)
-void subtract(array<int, MEMORY_SPACE>, int& loc, long long int& accum);
+void subtract(array<int, MEMORY_SPACE> mem, int& loc, long long int& accum);
 
 //  Code = 32
 //  divide() => divide a word from a location in memory into the
 //  word in the accumulator (leaving result in accumulator)
-void divide(array<int, MEMORY_SPACE>, int& loc, long long int& accum);
+void divide(array<int, MEMORY_SPACE> mem, int& loc, long long int& accum);
 
 //  Code = 33
 //  multiply() => multiply a word from a location in memory by the 
 //  word in accumulator (leaving result in accumulator)
-void multiply(array<int, MEMORY_SPACE>, int& loc, long long int& accum);
+void multiply(array<int, MEMORY_SPACE> mem, int& loc, long long int& accum);
 
 
 
 /*  Transfer-Of-Control Operations  */
 //  Code = 40
 //  branch() => branch to a specific location in memory
-int branch(int& loc);
+void branch(int& loc, int& newPos);
 
 //  Code = 41
 //  branchNeg() => branch to specific location in memory if accumulator
 //  is negative
-int branchNeg(int& loc, long long int& accum);
+void branchNeg(int& loc, long long int& accum, int& newPos);
 
 //  Code = 42
 //  branchzero() => branch to specific location in memory if accumulator
 //  is zero
-int branchZero(int& loc, long long int& accum);
+void branchZero(int& loc, long long int& accum, int& newPos);
 
 //  Code = 43
 // halt() => halt the program
