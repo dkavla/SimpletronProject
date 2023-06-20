0bf853629d92c7af7994f7f00be1fbb070b973af
dkavla <dkowalczyk135@gmail.com>
2023-06-19 23:20:55 -0400

    Complete declarations for operations

--- src/operations.h
+++ src/operations.h
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <array>
 using std::array;
-using std::cout;
 
 const size_t MEMORY_SPACE = 100; // space in memory; 100 words available
 
@@ -24,34 +23,51 @@ void write(array<int, MEMORY_SPACE>, int& loc);
 /*  Load/Store Operations   */
 //  Code = 20
 //  load() => load a word from location in memory into accumulator
-void load(array<int, MEMORY_SPACE>, int& loc);
+void load(array<int, MEMORY_SPACE>, int& loc, long long int& accum);
 
 //  Code = 21
 //  store() => stores a word from the accumulator into a specific location in memory
-void store(array<int, MEMORY_SPACE>, int& loc);
+void store(array<int, MEMORY_SPACE>, int& loc, long long int& accum);
 
 
 /*  Arithmetic Operations   */
 //  Code = 30
 //  add() => add a word from location in memory to the word in the
 //  accumulator (leaving result in accumulator)
-void add(array<int, MEMORY_SPACE>, int& loc);
+void add(array<int, MEMORY_SPACE>, int& loc, long long int& accum);
 
 //  Code = 31
 //  subtract() => subtract a word from location in memory from the
 //  word in accumulator (leaving result in accumulator)
-void subtract(array<int, MEMORY_SPACE>, int& loc);
+void subtract(array<int, MEMORY_SPACE>, int& loc, long long int& accum);
 
 //  Code = 32
 //  divide() => divide a word from a location in memory into the
 //  word in the accumulator (leaving result in accumulator)
-void divide(array<int, MEMORY_SPACE>, int& loc);
+void divide(array<int, MEMORY_SPACE>, int& loc, long long int& accum);
 
 //  Code = 33
 //  multiply() => multiply a word from a location in memory by the 
 //  word in accumulator (leaving result in accumulator)
-void multiply(array<int, MEMORY_SPACE>, int& loc);
+void multiply(array<int, MEMORY_SPACE>, int& loc, long long int& accum);
+
 
 
 /*  Transfer-Of-Control Operations  */
+//  Code = 40
+//  branch() => branch to a specific location in memory
+int branch(int& loc);
 
+//  Code = 41
+//  branchNeg() => branch to specific location in memory if accumulator
+//  is negative
+int branchNeg(int& loc, long long int& accum);
+
+//  Code = 42
+//  branchzero() => branch to specific location in memory if accumulator
+//  is zero
+int branchZero(int& loc, long long int& accum);
+
+//  Code = 43
+// halt() => halt the program
+void halt();
