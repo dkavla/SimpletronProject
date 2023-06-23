5031636b2e3c65dac36e6cee28a404494429b8e4
dkavla <dkowalczyk135@gmail.com>
2023-06-22 23:11:04 -0400

    Provide arguments to halt() function in switch statement case for code 43

--- src/main.cpp
+++ src/main.cpp
@@ -7,125 +7,89 @@ using std::array;
 
 long double accumulator = 0; // our register for holding values
 array<double, MEMORY_SPACE> memory; // memory of our Simpletron computer
-int location = 0; // tracks current location in memory
+int instructionCounter = 0; // tracks current location in memory
 
 int main() {
-    // Sentinel-controlled loop that reads positive numbers and computes
-    // and displays their sum
-    // memory[0] = 1008;
-    // memory[1] = 2008;
-    // memory[2] = 4106;
-    // memory[3] = 3009;
-    // memory[4] = 2109;
-    // memory[5] = 4000;
-    // memory[6] = 1109;
-    // memory[7] = 4300;
 
-
-    // Counter controlled loop that reads positive and negative numbers
-    // and then computes the average once counter equals zero
-    
-    memory[14] = 7; // counter variable
-    memory[15] = 7; // divisor for calculating average
-    memory[16] = 1; // decrementer; stays constant
-
-    // Instructions:
-    memory[0] = 1019; 
-    memory[1] = 2019; 
-    memory[2] = 3017;
-    memory[3] = 2117;
-    memory[4] = 2014;
-    memory[5] = 3116;
-    memory[6] = 4209; // branchZero
-    memory[7] = 2114;
-    memory[8] = 4000; // branch
-    memory[9] = 2017;
-    memory[10] = 3215;
-    memory[11] = 2118;
-    memory[12] = 1118;
-    memory[13] = 4300;
-
-
-    int operation; // holds the operation code
-    int operandLoc; // the operand location in memory
-    int instruction; // holds instruction, cast from double to int
+    int operationCode; // holds the operation code
+    int operand; // the operand location in memory
+    int instructionRegister; // holds instruction, cast from double to int
 
     while (true) {
-        instruction = static_cast<int>(memory[location]); 
-        operation = (instruction % 10000 - instruction % 100) / 100; // fetch operation code part of instruction
-        operandLoc = instruction % 100; // fetch location of operand part of instruction
+        instructionRegister = static_cast<int>(memory[instructionCounter]); 
+        operationCode = instructionRegister / 100; // fetch operation code part of instruction
+        operand = instructionRegister % 100; // fetch location of operand part of instruction
 
-        cout << "Current Operation Code = " << operation << "\n";
+        cout << "Current Operation Code = " << operationCode << "\n";
         // evaluate the operation code
-        switch (operation) {
+        switch (operationCode) {
             
             case 10:
-                read(memory, operandLoc);
-                cout << "Memory[" << operandLoc << "] = " << memory[operandLoc] << '\n';
-                location++;
+                read(memory, operand);
+                cout << "Memory[" << operand << "] = " << memory[operand] << '\n';
+                instructionCounter++;
                 break;
             case 11:
-                write(memory, operandLoc);
-                location++;
+                write(memory, operand);
+                instructionCounter++;
                 break;
             case 20:
                 cout << "Accumulator (Before load): " << accumulator << '\n';
-                load(memory, operandLoc, accumulator);
+                load(memory, operand, accumulator);
                 cout << "Accumulator (After load): " << accumulator << '\n';
-                location++;
+                instructionCounter++;
                 break;
             case 21:
                 cout << "Accumulator (Before store): " << accumulator << '\n';
-                store(memory, operandLoc, accumulator);
+                store(memory, operand, accumulator);
                 cout << "Accumulator (After store): " << accumulator << '\n';
-                location++;
+                instructionCounter++;
                 break;
             case 30:
                 cout << "Accumulator (Before add): " << accumulator << '\n';
-                add(memory, operandLoc, accumulator);
+                add(memory, operand, accumulator);
                 cout << "Accumulator (After add): " << accumulator << '\n';
-                location++;
+                instructionCounter++;
                 break;
             case 31:
                 cout << "Accumulator (Before subtract): " << accumulator << '\n';
-                subtract(memory, operandLoc, accumulator);
+                subtract(memory, operand, accumulator);
                 cout << "Accumulator (After subtract): " << accumulator << '\n';
-                location++;
+                instructionCounter++;
                 break;
             case 32:
                 cout << "Accumulator (Before divide): " << accumulator << '\n';
-                divide(memory, operandLoc, accumulator);
+                divide(memory, operand, accumulator);
                 cout << "Accumulator (After divide): " << accumulator << '\n';
-                location++;
+                instructionCounter++;
                 break;
             case 33:
                 cout << "Accumulator (Before multiply): " << accumulator << '\n';
-                multiply(memory, operandLoc, accumulator);
+                multiply(memory, operand, accumulator);
                 cout << "Accumulator (After multiply): " << accumulator << '\n';
-                location++;
+                instructionCounter++;
                 break;
             case 40:
-                cout << "Location (Before branch): " << location << '\n';
-                branch(location, operandLoc);
-                cout << "Location (After branch): " << location << '\n';
+                cout << "Location (Before branch): " << instructionCounter << '\n';
+                branch(instructionCounter, operand);
+                cout << "Location (After branch): " << instructionCounter << '\n';
                 break;
             case 41:
-                cout << "Location (Before branchNeg): " << location << '\n';
-                branchNeg(location, accumulator, operandLoc);
-                cout << "Location (After branchNeg): " << location << '\n';
+                cout << "Location (Before branchNeg): " << instructionCounter << '\n';
+                branchNeg(instructionCounter, accumulator, operand);
+                cout << "Location (After branchNeg): " << instructionCounter << '\n';
                 break;
             case 42:
-                cout << "Location (Before branchZero): " << location << '\n';
-                branchZero(location, accumulator, operandLoc);
-                cout << "Location (After branchZero): " << location << '\n';
+                cout << "Location (Before branchZero): " << instructionCounter << '\n';
+                branchZero(instructionCounter, accumulator, operand);
+                cout << "Location (After branchZero): " << instructionCounter << '\n';
                 break;
             case 43:
-                cout << "Program Terminated.\n";
-                halt();
+                halt(accumulator, instructionCounter, instructionRegister, operationCode, operand);
             default:
                 cout << "Error: Invalid operation code input!\n";
                 exit(1);
         }
-        cout << "End of Operation Code " << operation << "\n\n";
+        cout << "End of Operation Code " << operationCode << "\n\n";
     } 
 }
