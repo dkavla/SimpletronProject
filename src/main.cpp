94c3ab5a428fc3777dffa0306d764f8ea50ab717
dkavla <dkowalczyk135@gmail.com>
2023-07-13 20:42:58 -0400

    Remove helper statements from switch cases

--- src/main.cpp
+++ src/main.cpp
@@ -54,7 +54,6 @@ int main() {
         operationCode = instructionRegister / 100; // fetch operation code part of instruction
         operand = instructionRegister % 100; // fetch location of operand part of instruction
 
-        cout << "Current Operation Code = " << operationCode << "\n";
         // evaluate the operation code
         switch (operationCode) {
             
@@ -68,67 +67,54 @@ int main() {
                 instructionCounter++;
                 break;
             case 20:
-                cout << "Accumulator (Before load): " << accumulator << '\n';
                 load(memory, operand, accumulator);
-                cout << "Accumulator (After load): " << accumulator << '\n';
                 instructionCounter++;
                 break;
             case 21:
-                cout << "Accumulator (Before store): " << accumulator << '\n';
                 store(memory, operand, accumulator);
-                cout << "Accumulator (After store): " << accumulator << '\n';
                 instructionCounter++;
                 break;
             case 30:
-                cout << "Accumulator (Before add): " << accumulator << '\n';
                 add(memory, operand, accumulator);
-                cout << "Accumulator (After add): " << accumulator << '\n';
                 instructionCounter++;
                 break;
             case 31:
-                cout << "Accumulator (Before subtract): " << accumulator << '\n';
                 subtract(memory, operand, accumulator);
-                cout << "Accumulator (After subtract): " << accumulator << '\n';
                 instructionCounter++;
                 break;
             case 32:
-                cout << "Accumulator (Before divide): " << accumulator << '\n';
                 try {
                     divide(memory, operand, accumulator);
                 } catch (std::exception e) {
                     cout << "*** Attempt to divide by zero ***\n*** Simpletron execution abnormally terminated ***\n";
                     halt(memory, accumulator, instructionCounter, instructionRegister, operationCode, operand);
                 }
-                cout << "Accumulator (After divide): " << accumulator << '\n';
                 instructionCounter++;
                 break;
             case 33:
-                cout << "Accumulator (Before multiply): " << accumulator << '\n';
                 multiply(memory, operand, accumulator);
-                cout << "Accumulator (After multiply): " << accumulator << '\n';
                 instructionCounter++;
                 break;
             case 40:
-                cout << "Location (Before branch): " << instructionCounter << '\n';
                 branch(instructionCounter, operand);
-                cout << "Location (After branch): " << instructionCounter << '\n';
                 break;
             case 41:
-                cout << "Location (Before branchNeg): " << instructionCounter << '\n';
                 branchNeg(instructionCounter, accumulator, operand);
-                cout << "Location (After branchNeg): " << instructionCounter << '\n';
                 break;
             case 42:
-                cout << "Location (Before branchZero): " << instructionCounter << '\n';
                 branchZero(instructionCounter, accumulator, operand);
-                cout << "Location (After branchZero): " << instructionCounter << '\n';
                 break;
             case 43:
                 halt(memory, accumulator, instructionCounter, instructionRegister, operationCode, operand);
             default:
                 cout << "Error: Invalid operation code input!\n";
+                halt(memory, accumulator, instructionCounter, instructionRegister, operationCode, operand);
                 exit(1);
         }
-        cout << "End of Operation Code " << operationCode << "\n\n";
+        if (accumulator > 9999 || accumulator < -9999) {
+            cout << "*** Accumulator Overflow. Terminating Program. ***\n";
+            halt(memory, accumulator, instructionCounter, instructionRegister, operationCode, operand);
+            break;
+        }
     } 
 }
